db337d2ca59fdc3a465edd78cc7720a3a985847a
Greg Slepak <contact@taoeffect.com>
2026-05-12 20:07:36 -0700

    Remove obsolete store mutation helpers

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import "./view";
 import "./controller";
 import "./model";
 import App from "./App.vue";
-import { useThreadStore } from "./model/state/thread";
 import { useTabStore } from "./model/state/tabs";
 import { useSettingsStore } from "./model/state/settings";
 import { usePromptsStore } from "./model/state/prompts";
@@ -18,7 +17,6 @@ window.sbp = sbp;
 
 if (import.meta.env.DEV) {
   (window as unknown as { __stores: unknown }).__stores = {
-    thread: useThreadStore(),
     tab: useTabStore(),
     settings: useSettingsStore(),
     prompts: usePromptsStore(),
